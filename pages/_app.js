c92757700221950b60d97be7421db4c3eacfcb1b
Faiz Ahmed <4faizahmed@gmail.com>
2022-08-16 08:42:07 +0300

    Project configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import '../styles/globals.scss'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Head>
+          <title>Tour Guide</title>
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <meta name="format-detection" content="telephone=no"/>
+          <meta name="description" content="An information interface for tourist"/>
+          <meta name="keywords" content="tour guide, tourist, travel, places"/>
+          <meta name="author" content="Faiz Ahmed"/>
+        </Head>
+        <Component {...pageProps} />
+      </>
+      )
+
 }
 
 export default MyApp
